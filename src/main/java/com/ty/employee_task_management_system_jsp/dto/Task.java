34bf950819b3e2bcd2f284688ec9d6dd9c204c92
javeed762001 <javeed762001@gmail.com>
2023-12-16 15:30:58 +0530

    firstcommit

---
+++ src/main/java/com/ty/employee_task_management_system_jsp/dto/Task.java
@@ -0,0 +1,28 @@
+package com.ty.employee_task_management_system_jsp.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Task {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String description;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
